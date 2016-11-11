e86f82666f29c36b6e9913fe2dcf992ecc2dab7c
pguimaraes1234567890 <dragon_killer_68@hotmail.fr>
2016-11-11 21:12:42 +0100

    zombie color

--- Assets/Script/ZombieFaction.cs
+++ Assets/Script/ZombieFaction.cs
@@ -4,11 +4,19 @@ using System.Collections;
 public class ZombieFaction : MonoBehaviour {
     public uint faction;
     private static uint nbFaction;
+    private ArrayList colorList = new ArrayList{Color.red,Color.green,Color.black,Color.blue, Color.yellow};
+    private Random rnd = new Random();
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         faction = nbFaction;
         nbFaction++;
-	}
+        MeshRenderer m = gameObject.GetComponentInChildren<MeshRenderer>();
+        m.enabled = true;
+
+        m.material.color = (Color)colorList[(int)faction];
+    }
+
+    
 }
