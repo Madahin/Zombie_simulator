b5fafc4234cd938729ee95d8cb1c4cdc0ebb1a50
pguimaraes1234567890 <dragon_killer_68@hotmail.fr>
2016-11-11 20:22:50 +0100

    fraking menu

---
+++ Assets/Script/menu/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour{
+
+    public void LoadStage(string yourStage)
+    {
+        Application.LoadLevel(yourStage);
+    }
+
+    public void LoadStage()
+    {
+        Application.LoadLevel("Main");
+    }
+
+}
