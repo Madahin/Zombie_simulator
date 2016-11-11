20c3fb629b8c37a5762ea786c63cadc9e370eb42
Madahin <nico99999@hotmail.fr>
2016-11-11 13:55:29 +0100

    Add the procedural generator script + fbx directory

---
+++ Assets/Script/ProceduralMapGeneration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectBuilderScript))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        if (GUILayout.Button("Build Object"))
+        {
+            Debug.Log("patate");
+        }
+    }
+}
